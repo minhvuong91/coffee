50f56e702a76898644889d4ec8c2c657e93fc421
Minh Vuong <minhvuong@Vuongs-MacBook-Pro.local>
2023-08-05 10:55:30 +0700

    sad

---
+++ coffee_phamhuythong/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace coffee_phamhuythong;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Huy them kiu");
+    }
+}
+
---
+++ coffee_phamhuythong/huy_them_kiu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace coffee_phamhuythong
+{
+	public class huy_them_kiu
+	{
+		string Huy = "";
+		int kiu = 15;
+	}
+}
+
